13bf974878d0dcbe32c366867a15604480f97eb1
Alexander <Alexander>
2016-04-16 13:16:35 +0200

    Sketch for challenge 1: Potentiometer

---
+++ arduinoAdventures/challenge1/challenge1.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // setup serial port
+  Serial.begin(9600);
+}
+
+void loop() {
+  // set sensorValue to the reading on analog input 0
+  int sensorValue = analogRead(A0);
+  // set the mappedSensorValue to the value of the map() function
+  int mappedSensorValue = map(sensorValue, 0, 1023, 0, 9);
+  // send mappedSensorValue to the serial port.
+  Serial.println(mappedSensorValue, DEC);
+}
