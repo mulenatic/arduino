5ac821747f39db8ad58526c0af176f45a90fe290
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-06 12:26:26 +0200

    New sketch for the Unreliable switch from the book

---
+++ arduinoIntro/ch03UnreliableSwitch/ch03UnreliableSwitch.ino
@@ -0,0 +1,22 @@
+const unsigned int BUTTON_PIN = 2;
+const unsigned int LED_PIN = 12;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+
+}
+
+int led_state = LOW;
+void loop() {
+  const int CURRENT_BUTTON_STATE = digitalRead(BUTTON_PIN);
+  if (CURRENT_BUTTON_STATE == HIGH) {
+    if (led_state == LOW) {
+      led_state = HIGH;
+    } else {
+      led_state = LOW;
+    }
+    digitalWrite(LED_PIN, led_state);
+  }
+
+}
