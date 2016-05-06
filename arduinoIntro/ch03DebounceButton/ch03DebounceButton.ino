e26dcc72599cc7bb807a29022a1cfc83835097e5
Thomas Kaczmarek <mulenatic@gmail.com>
2016-05-06 12:34:51 +0200

    Final version for the button things, now it behaves as expected by waiting a bit after the led_state ischanged. This prevents the button bounce to be recognized.

---
+++ arduinoIntro/ch03DebounceButton/ch03DebounceButton.ino
@@ -0,0 +1,25 @@
+const unsigned int BUTTON_PIN = 2;
+const unsigned int LED_PIN = 12;
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+}
+
+int old_button_state = LOW;
+int led_state = LOW;
+
+void loop() {
+  const int CURRENT_BUTTON_STATE = digitalRead(BUTTON_PIN);
+  if (CURRENT_BUTTON_STATE != old_button_state && CURRENT_BUTTON_STATE == HIGH) {
+    if (led_state == LOW) {
+      led_state = HIGH;
+    } else {
+      led_state = LOW;
+    }
+    digitalWrite(LED_PIN, led_state);
+    delay(50);
+  }
+  old_button_state = CURRENT_BUTTON_STATE;
+}
